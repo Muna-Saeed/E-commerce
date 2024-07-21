7ff0aabdf619af9ac60260efd4aee4f7982ac929
Muna Saeed <munasaeed6367@gmail.com>
2024-07-21 03:18:31 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -233,6 +233,32 @@ app.get('/popularinwomen', async(req,res) => {
     res.send(popular_in_women);
 })
 
+//creating middleware to fetch user
+const fetchUser = async(req,res,next) => {
+    const token = req.header('auth-token')
+    if(!token){
+        res.status(401).send({errors:"please authenticate using a valid token"})
+    }
+    else{
+        try{
+            const data = jwt.verify(token, 'secret-ecom');
+            req.user =data.user;
+            next();
+        }catch (error){
+            res.status(401).send({errors:"please authenticate using a valid token"})
+        }
+    }
+}
+
+//creating endpoint for adding product in cartdata
+app.post('/addtocart', fetchUser, async(req,res)=>{
+    console.log(req.body,req.user);
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
+    res.send("Added")
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server Running on Port " + port);
